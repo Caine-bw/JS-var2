382ff3709b7f867d8dee6da598e810b9b3bc9df6
Abdel <elb.abd@icloud.com>
2021-01-14 00:35:46 +0100

    Exo3

---
+++ main.js
@@ -0,0 +1,32 @@
+let tartine = ("j'aime les tartines et j'en mange");
+console.log(tartine);
+
+let jours =("4 par jours");
+console.log(jours);
+
+console.log(tartine + " " + jours);
+
+let nombre = 5;
+console.log (nombre);
+
+let nombre2 = 35;
+console.log (nombre2);
+
+console.log(nombre + " " + nombre2);
+
+console.log(tartine + " " + nombre);
+//Exo 2
+let hey =("Salut tout le monde");
+console.log(hey);
+
+let bye =("Aurevoir!");
+console.log(hey = bye);
+
+//Exo 3
+let po = 4;
+console.log(po);
+
+let pop = ("6");
+console.log(po > pop);
+
+
